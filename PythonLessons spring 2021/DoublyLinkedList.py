1be68fa22b1839413575ec4e7cf420b7375a2b9e
Moroz Kostimo <kostyamoroz50@gmail.com>
2021-03-21 17:16:51 +0200

    DL_List with pushing nodes to the begining (v2)

--- PythonLessons spring 2021/DoublyLinkedList.py
+++ PythonLessons spring 2021/DoublyLinkedList.py
@@ -11,27 +11,21 @@ class DoublyLinkedList:
     
     def __init__(self):
         self.head = None
-        self.tail = None
         self.length = 0
     
-    # Добавление элементов в начало списка
+    # Вставка узлов в начало списка
     def push(self, cat):
-        new_box = Box(cat, self.head, None)
-        if self.head == None:
-            self.head = new_box
-        elif self.tail == None:
-            self.tail = new_box
-            self.head.prev = self.tail 
-        else:
-            self.head = self.tail
-            self.tail = new_box
-            self.head.prev = self.tail
-            self.tail.next = self.head
+        newBox = Box(cat)
+        newBox.next = self.head
+        if self.head != None:
+            self.head.prev = newBox
+        self.head = newBox
         self.length += 1
-
 
 lst = DoublyLinkedList()
 lst.push("qwerty")
 lst.push("lorem")
 lst.push("ipsum")
-print(lst)
+# lst.insert(2, "amet")
+# lst.insert(2, "end")
+# lst.output(lst.head)
