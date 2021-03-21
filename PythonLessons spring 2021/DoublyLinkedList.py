8782868d942be5470c86359a4dde3b50ab72282a
Moroz Kostimo <kostyamoroz50@gmail.com>
2021-03-21 22:13:06 +0200

    final version

--- PythonLessons spring 2021/DoublyLinkedList.py
+++ PythonLessons spring 2021/DoublyLinkedList.py
@@ -22,10 +22,81 @@ class DoublyLinkedList:
         self.head = newBox
         self.length += 1
 
+    # Вставка узлов по индексу
+    def insert(self, index_prevBox, cat):
+        newBox = Box(cat)
+        counter = 0
+        pointer = self.head
+        while pointer != None:
+            if counter == index_prevBox:
+                newBox.next = pointer.next
+                pointer.next = newBox
+                newBox.prev = pointer
+            if counter == index_prevBox + 2:
+                pointer.prev = newBox
+                break
+            pointer = pointer.next
+            counter += 1
+        self.length += 1
+
+    # Удаление узлов по индексу             ipsum lorem qwerty end amet
+    def remove(self, index):
+        counter = 0
+        pointer = self.head
+        while pointer.next != None:
+            if counter + 1 == index:
+                pointer.next = pointer.next.next
+                if pointer.next != None:
+                    pointer.next.prev = pointer
+                break
+            pointer = pointer.next
+            counter += 1
+        print(f"The necessary cat is not on this list ")
+    
+    # Поиск узлов по индексу
+    def index_search(self, index):
+        counter = 0
+        pointer = self.head
+        while pointer.next != None:
+            if counter == index:
+                return pointer.cat
+                break
+            pointer = pointer.next
+            counter += 1
+        return f"The necessary cat is not on this list "
+
+    # Поиск узлов по значению
+    def value_search(self, cat):
+        counter = 0
+        pointer = self.head
+        while pointer.next != None:
+            if pointer.cat == cat:
+                return f"Cat \'{pointer.cat}\' has been found by index {counter}"
+            pointer = pointer.next
+            counter += 1
+        return f"The necessary cat is not on this list "
+
+
+    # Вывод списка
+    def output(self, box):
+        print("----------")
+        while box != None:
+            print(box.cat)
+            box = box.next
+
 lst = DoublyLinkedList()
 lst.push("qwerty")
 lst.push("lorem")
 lst.push("ipsum")
-# lst.insert(2, "amet")
-# lst.insert(2, "end")
-# lst.output(lst.head)
+lst.insert(2, "amet")
+lst.insert(2, "end")
+lst.output(lst.head)
+lst.remove(3)
+lst.output(lst.head)
+lst.remove(3)
+lst.output(lst.head)
+lst.remove(3)
+print(lst.index_search(0))
+print(lst.index_search(-1))
+print(lst.value_search("ipsum"))
+print(lst.value_search("sit"))
